14c68db72fd0448dd559212e55485ed28df7b40a
Aditya <adityabasak7031@gmail.com>
2023-03-31 16:55:00 +0530

    Modified weather App/main remove my API Key You have to add your own key

--- Weather App/main.py
+++ Weather App/main.py
@@ -6,7 +6,7 @@ speak = wincom.Dispatch("SAPI.SpVoice")
 
 city = input("Enter the name of the city: ")
 
-url = f"https://api.weatherapi.com/v1/current.json?key=366ad543683f40f9b30191246233003&q={city}"
+url = f"https://api.weatherapi.com/v1/current.json?key=YOUR_API_kEY&q={city}"
 
 r = requests.get(url)
 # print(r.text)
@@ -21,4 +21,3 @@ humid = weatherDic["current"]["humidity"]
 
 speak.Speak(f" Currently the Temperature of {city} is {temp} degrees, feels like{feelsLike} degrees. predictable "
             f"weather is {predictableWeather}, wind is {wind} kilometer per hour and humidity is {humid}")
-
